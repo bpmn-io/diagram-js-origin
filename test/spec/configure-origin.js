82c97891ef12f24723550675bef8194f6c119d40
pedesen <patrick.dehn@camunda.com>
2016-11-07 17:49:20 +0100

    fix(origin): append properly
    Closes camunda/camunda-modeler#439

--- test/spec/configure-origin.js
+++ test/spec/configure-origin.js
@@ -5,6 +5,7 @@ require('diagram-js/test/helper');
 
 var originModule = require('../../');
 
+var innerSVG = require('tiny-svg/lib/innerSVG');
 
 describe('origin', function() {
 
@@ -14,6 +15,14 @@ describe('origin', function() {
 
   it('should bootstrap', inject(function(canvas) {
     canvas.scroll({ dx: 100, dy: 100 });
+  }));
+
+
+  it('should exist in canvas', inject(function(canvas) {
+    var layer = canvas.getLayer('bg');
+    var text = layer.querySelector('text');
+
+    expect(innerSVG(text)).to.equal('(0, 0)');
   }));
 
 });
