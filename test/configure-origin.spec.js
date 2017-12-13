9237c78646a9629ed1440034dc6aa7e53fd8229a
Nico Rehwaldt <git_nikku@nixis.de>
2017-12-14 00:53:25 +0100

    feat(origin): add additional horizontal / vertical lines
    * optional horizontal / vertical separators show
      where canvas positive coordinate space is.

--- test/configure-origin.spec.js
+++ test/configure-origin.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('diagram-js/test/helper');
 
 /* global bootstrapDiagram, inject */
@@ -10,20 +12,61 @@ var innerSVG = require('tiny-svg/lib/innerSVG');
 
 describe('origin', function() {
 
-  var testModules = [ originModule ];
+  describe('basic', function() {
 
-  beforeEach(bootstrapDiagram({ modules: testModules }));
+    beforeEach(bootstrapDiagram({
+      modules: [
+        originModule
+      ]
+    }));
 
-  it('should bootstrap', inject(function(canvas) {
-    canvas.scroll({ dx: 100, dy: 100 });
-  }));
+    it('should bootstrap', inject(function(canvas) {
+      canvas.scroll({ dx: 100, dy: 100 });
+    }));
 
 
-  it('should exist in canvas', inject(function(canvas) {
-    var layer = canvas.getLayer('bg');
-    var text = layer.querySelector('text');
+    it('should exist in canvas', inject(function(canvas) {
 
-    expect(innerSVG(text)).to.equal('(0, 0)');
-  }));
+      // given
+      var layer = canvas.getLayer('bg');
+
+      var label = layer.querySelector('.djs-origin-label'),
+          cross = layer.querySelector('.djs-origin-cross'),
+          border = layer.querySelector('.djs-origin-border');
+
+      // then
+      expect(label).to.exist;
+      expect(cross).to.exist;
+      expect(border).to.exist;
+
+      expect(innerSVG(label)).to.equal('(0, 0)');
+    }));
+
+  });
+
+  describe('no border', function() {
+
+    beforeEach(bootstrapDiagram({
+      modules: [
+        originModule,
+        {
+          config: [ 'value', { origin: { showBorder: false } } ]
+        }
+      ]
+    }));
+
+
+    it('should hide border', inject(function(canvas) {
+
+      // given
+      var layer = canvas.getLayer('bg');
+
+      var border = layer.querySelector('.djs-origin-border');
+
+      // then
+      expect(border).not.to.exist;
+    }));
+
+  });
 
 });
